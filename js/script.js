4cbe445463f23f1c0c27ffb4bc689a204919c64e
oduvanka <qcatjul@gmail.com>
2019-06-11 23:29:24 +0400

    Корректирует обработку событий закрытия окна по крестике, добавляет события при клике на кнопки в мини-карточке товара

--- js/script.js
+++ js/script.js
@@ -1,13 +1,34 @@
-let modalAll = document.querySelectorAll(".modal");
-  btnShowMap = document.querySelector(".contacts-map"),
-  btnCreateMessage = document.querySelector(".btn-create-message"),
-  btnAddCartAll = document.querySelectorAll(".btn-add-cart"),
-  btnCloseModalAll = document.querySelectorAll(".btn-close");
+var tabCart = document.querySelector(".main-header .cart");
+  tabBookmark = document.querySelector(".main-header .bookmark");
+  counterCart = tabCart.querySelector("span");
+  counterBookmark = tabBookmark.querySelector("span");
 
-let isStorageSupport = true;
+var btnShowMap = document.querySelector(".contacts-map"),
+  popupMap = document.querySelector(".modal-map");
 
-let storageName = "",
-  storageEmail = "";
+var btnCreateMessage = document.querySelector(".btn-create-message"),
+  popupMessage = document.querySelector(".modal-message"),
+  formMessage,
+  inputName,
+  inputEmail,
+  inputText;
+
+var listCatalog = document.querySelector(".catalog-list"),
+  popupInformer = document.querySelector(".modal-informer");
+
+var isStorageSupport = true;
+
+var storageName = "",
+  storageEmail = "",
+  numberCart = Number(counterCart.textContent),
+  numberBookmark = Number(counterBookmark.textContent);
+
+if (popupMessage) {
+  formMessage = popupMessage.querySelector(".modal-message-form");
+  inputName = formMessage.querySelector(".message-input-name");
+  inputEmail = formMessage.querySelector(".message-input-email");
+  inputText = formMessage.querySelector(".message-input-text");
+}
 
 /* Проверяет доступность localStoradge */
 try {
@@ -23,118 +44,114 @@ if (btnShowMap) {
   btnShowMap.addEventListener("click", function(evt) {
     evt.preventDefault();
 
-    let modalMap = document.querySelector(".modal-map");
-    showModal(modalMap);
+    showPopup(popupMap);
   });
 }
 
 if (btnCreateMessage) {
-  addEvtToBtnMessage();
+  btnCreateMessage.addEventListener("click", createMessage);
 }
 
-if (btnAddCartAll.length > 0) {
-  addEvtToBtnAddCart(btnAddCartAll);
+if (listCatalog) {
+  listCatalog.addEventListener("click", clickCatalog);
 }
 
-if (btnCloseModalAll.length > 0) {
-  addEvtToBtnCloseModal(btnCloseModalAll);
-}
-
-/* Добавляет обработчик клика на кнопку Напишите нам */
-function addEvtToBtnMessage() {
-  btnCreateMessage.addEventListener("click", function(evt) {
+/* Функция для обработчика клика на кнопку Напишите нам */
+function createMessage(evt) {
     evt.preventDefault();
-    
-    let modalMessage = document.querySelector(".modal-message"),
-      formMessage = modalMessage.querySelector(".modal-message-form"),
-      inputName = formMessage.querySelector(".message-input-name"),
-      inputEmail = formMessage.querySelector(".message-input-email"),
-      inputText = formMessage.querySelector(".message-input-text"),
-      btnSendMessage = formMessage.querySelector(".btn-send-message");
 
-    showModal(modalMessage);
+    showPopup(popupMessage);
 
     fillInput(inputName, storageName);
     fillInput(inputEmail, storageEmail);
     
+    inputText.focus();
     if (!storageName) {
       inputName.focus();
     }
-    else {
-      if (!storageEmail) {
-        inputEmail.focus();
-      }
-      else {
-        inputText.focus();
-      }
+    else if (!storageEmail) {
+      inputEmail.focus();
     }
     
-    addEvtToSendMessage(formMessage);
-  });
+    formMessage.addEventListener("submit", sendMessage);
 }
 
-function addEvtToSendMessage(formMessage) {
-  /* Отслеживает отправку формы с сообщением */
-  formMessage.addEventListener("submit", function(evt) {
-    let inputName = formMessage.querySelector(".message-input-name"),
-      inputEmail = formMessage.querySelector(".message-input-email"),
-      inputText = formMessage.querySelector(".message-input-text");
+/* Функция для обработчика отправки формы сообщения */
+function sendMessage(evt) {
+  evt.preventDefault();
 
-    if (!inputName.value || !inputEmail.value || !inputText.value) {
-      evt.preventDefault();
+  if (!inputName.value || !inputEmail.value || !inputText.value) {
+    popupMessage.classList.remove("modal-error");
+    popupMessage.offsetWidth = popupMessage.offsetWidth;
+    popupMessage.classList.add("modal-error");
+  }
+  else {
+    if (isStorageSupport) {
+      localStorage.setItem("technomartMessageName", inputName.value);
+      localStorage.setItem("technomartMessageEmail", inputEmail.value);
     }
-    else {
-      if (isStorageSupport) {
-        localStorage.setItem("technomartMessageName", inputName.value);
-        localStorage.setItem("technomartMessageEmail", inputEmail.value);
-        hideModal(modalMessage);
-      }
-    }
-  });
+    hidePopup(popupMessage);
+  }
+};
+
+/* Функция добавляет окну класс, который делает его видимым, и назначает обработчики закрытия окна
+myPopup - модальное окно*/
+function showPopup(myPopup) {
+  myPopup.classList.add("modal-show");
+
+  var btnClosePopup = myPopup.querySelector(".btn-close");
+
+  if (btnClosePopup) {
+    btnClosePopup.addEventListener("click", onClickBtnClosePopup);
+  }
+  window.addEventListener("keydown", keydownClosePopup);
 }
 
-/* Функция рекурсивно добавляет обработчик клика на кнопку В корзину
-arrBtns - все найденные кнопки В корзину,
-btnN - номер текущей кнопки */
-function addEvtToBtnAddCart(arrBtns, btnN=0) {
-  arrBtns[btnN].addEventListener("click", function(evt) {
+/* Функция для обработчика клика по кнопке Закрыть */
+function onClickBtnClosePopup(evt) {
+  evt.preventDefault();
+
+  hidePopup(evt.target.closest(".modal"));
+}
+
+/* Функция для обработчика нажатия по клавише Esc */
+function keydownClosePopup(evt) {
+  if (evt.keyCode === 27) {
     evt.preventDefault();
-    
-    let modalInformer = document.querySelector(".modal-informer");
-    showModal(modalInformer);
-  });
-  btnN++;
-  if (btnN < arrBtns.length) {
-    addEvtToBtnAddCart(arrBtns, btnN);
+
+    /*if (myPopup.classList.contains("modal-show")) {
+      hidePopup(myPopup);
+    }*/
   }
 }
 
-/* Функция рекурсивно добавляет обработчик на кнопку Закрыть окно,
-arrBtns - все найденные кнопки Закрыть окно,
-btnN - номер текущей кнопки */
-function addEvtToBtnCloseModal(arrBtns, btnN=0) {
-  arrBtns[btnN].addEventListener("click", function(evt) {
-    evt.preventDefault();
-    
-    let parentModal = evt.target.parentElement.closest(".modal");
-    hideModal(parentModal);
-  });
-  btnN++;
-  if (btnN < arrBtns.length) {
-    addEvtToBtnCloseModal(arrBtns, btnN);
-  }
-}
+/* Функция для обработчика клика по каталогу товаров */
+function clickCatalog(evt) {
+  evt.preventDefault();
 
-/* Функция добавляет окну класс, который делает его видимым,
-myModal - модальное окно*/
-function showModal(myModal) {
-  myModal.classList.add("modal-show");
+  if (evt.target.classList.contains("btn-add-cart")) {
+    incCounterTab(numberCart, counterCart);
+
+    tabCart.classList.add("cart-full");
+
+    showPopup(popupInformer);
+  }
+  else if (evt.target.classList.contains("btn-add-bookmark")) {
+    incCounterTab(numberBookmark, counterBookmark);
+
+    tabBookmark.classList.add("bookmark-full");
+  }
 }
 
 /* Функция добавляет окну класс, который скрывает его,
-myModal - модальное окно*/
-function hideModal(myModal) {
-  myModal.classList.remove("modal-show");
+myPopup - модальное окно*/
+function hidePopup(myPopup) {
+  myPopup.classList.remove("modal-show");
+  myPopup.classList.remove("modal-error");
+
+  formMessage.removeEventListener("submit", sendMessage);
+  /*btnClosePopup.removeEventListener("click", onClickBtnClosePopup);*/
+  window.removeEventListener("keydown", keydownClosePopup);
 }
 
 /* Функция заполняет значение в поле ввода 
@@ -144,4 +161,12 @@ function fillInput(myInput, myValue) {
   if (myValue) {
     myInput.value = myValue;
   }
+}
+
+/* Функция вычисляет новое значение счётчика и обновляет его на вкладке,
+myNumber - текущее значение счётчика,
+myCounterTab - вкладка со счётчиком */
+function incCounterTab(myNumber, myCounterTab) {
+  myNumber++;
+  myCounterTab.textContent = myNumber;
 }
